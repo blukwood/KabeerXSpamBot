0c88146eeed769338dea03082248bddd2c84b66b
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-16 16:25:31 +0530

    Update raid.py

--- kabeer/pl/raid.py
+++ kabeer/pl/raid.py
@@ -5,12 +5,12 @@ from pyrogram.types import Chat, User
 from typing import ClassVar, Optional
 from pyrogram.types import Message
 import pyrogram
-from kabeer import GOD_USERS
+from config import Config
 from typing import Sequence
 
 
 
-
+GOD_USERS = Config.GOD_USERS
 
 
 @kabeercmd.on_message(filters.command('raid'))
