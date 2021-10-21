8a7de77c8958d676326ba42a69d3f4a4421febcc
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-21 17:13:42 +0530

    Update __main__.py

--- kabeer/__main__.py
+++ kabeer/__main__.py
@@ -1,4 +1,12 @@
-from kabeer import kabeercmd
+from kabeer import kabeercmd, vcraidcmd
+from kabeer import Config
 
+LOGGER = Config.LOG_CHANNEL
 if __name__ == '__main__':
+    print("KABEER : STARTING BOT")
     kabeercmd.run()
+    print("KABEER : STARTING VC RAIDER")
+    vcraidcmd.run()
+    print("STARTED PLEASE SEE LOG CHANNEL FOR MORE INFO.")
+    kabeercmd.send_message("BOT STARTED", LOGGER)
+    kabeercmd.send_message("VC RAIDER STARTED" LOGGER)
