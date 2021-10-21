493d2a8be4b619d71ebb6520edabf1bad1dd5613
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-21 19:27:35 +0530

    Update vcraid.py

--- kabeer/pl/vcraid.py
+++ kabeer/pl/vcraid.py
@@ -11,7 +11,26 @@ from typing import Sequence
 from typing import List, Union
 import random
 
+
+import pytgcalls 
+calls = pytgcalls.GroupCallFactory(vcraidcmd).get_group_call()
+
 @kabeercmd.on_message(filters.command('vcraid'))
-@vcraidcmd.on_message(filters.command('vcraid'))
-async def vcraid(client: Client, message: Message):
-    await message.reply("Hey", parse_mode=None)
+async def vcraid(_,message):
+  try:
+    await vcraidcmd.start()
+  except:
+    pass
+  reply = message.reply_to_message
+  if reply:
+    msg = await message.reply('Processing...')
+    path = await reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_audio(path , repeat=False)
+    await msg.edit('AAJA VC BSDK, MAI BOLO GA AAB')
+    
+    
+@kabeercmd.on_message(filters.command('stopvcraid'))    
+async def stopvcraid(_,message):
+  await calls.stop()
+  await calls.leave_current_group_call()
