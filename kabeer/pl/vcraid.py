e77c7fae8c416e98a255e4a80ed300dfe58c8148
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-21 17:23:28 +0530

    Update vcraid.py

--- kabeer/pl/vcraid.py
+++ kabeer/pl/vcraid.py
@@ -1,1 +1,15 @@
+from pyrogram import filters, Client
+import asyncio
+from kabeer import kabeercmd, vcraidcmd
+from pyrogram.types import Chat, User
+from typing import ClassVar, Optional
+from pyrogram.types import Message
+import pyrogram
+from kabeer.config import Config
+from typing import Sequence
 
+from typing import List, Union
+import random
+
+@kabeercmd.on_message(filters.command('vcraid'))
+@vcraidcmd.on_message(filters.command('raid'))
