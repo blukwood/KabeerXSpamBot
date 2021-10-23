aa4719fb065be62605c0a7909ca1bd1ef6996783
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-23 08:34:04 +0530

    Update __main__.py

--- kabeer/__main__.py
+++ kabeer/__main__.py
@@ -1,9 +1,9 @@
-from kabeer import kabeercmd
+from kabeer import exploiter
 from kabeer import Config
 
 LOGGER = Config.LOG_CHANNEL
 if __name__ == '__main__':
     print("KABEER : STARTING BOT")
-    kabeercmd.run()
+    exploiter.run()
     print("STARTED PLEASE SEE LOG CHANNEL FOR MORE INFO.")
     kabeercmd.send_message(LOGGER, "BOT STARTED")
